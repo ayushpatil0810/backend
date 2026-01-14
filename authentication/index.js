e2c798e588ee8c5e2bb4e88a68a0de454ed339ad
Ayush Patil <94798136+ayushpatil0810@users.noreply.github.com>
2026-01-14 21:59:11 +0530

    chore(authentication): initialize package.json with express and types

---
+++ authentication/index.js
@@ -0,0 +1,46 @@
+import express from 'express'
+
+const app = express()
+const PORT = 8000
+
+app.use(express.json())
+
+const DIARY = {
+
+}
+
+const EMAILS = new Set()
+
+app.post('/signup', (req, res) => {
+    const { name, email, password } = req.body
+
+    if(EMAILS.has(email)) {
+        return res.status(400).json({error: 'Email already taken'})
+    }
+
+    // Create a token for user
+    const token = `${Date.now()}`
+    DIARY[token] = {name, email, password}
+    EMAILS.add(email)
+    res.json({status: 'success', token})
+})
+
+app.post('/me', (req, res) => {
+    const { token } = req.body
+
+    if(!token) {
+        return res.status(400).json({error: 'Missing Token'})
+    }
+
+    if(!(token in DIARY)) {
+        return res.status(400).json({error: 'Invalid Token'})
+    }
+
+    const entry = DIARY[token]
+
+    return res.json({data: entry})
+})
+
+
+app.listen(PORT, console.log(`Server started on PORT on ${PORT}`))
+
