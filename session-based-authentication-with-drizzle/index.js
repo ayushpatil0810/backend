a13c91e3bb309a702fa777c96b889ee28f5e504f
Ayush Patil <94798136+ayushpatil0810@users.noreply.github.com>
2026-01-18 14:49:21 +0530

    feat(authentication): implement session-based user retrieval and update functionality

--- session-based-authentication-with-drizzle/index.js
+++ session-based-authentication-with-drizzle/index.js
@@ -1,10 +1,44 @@
 import express from "express"
 import router from "./routes/users.routes.js"
 
+
+import { eq } from 'drizzle-orm'
+import db from './db/index.js'
+import {usersTable, userSessions} from './db/schema.js'
+
 const app = express()
 const PORT = process.env.PORT ?? 8000
 
 app.use(express.json())
+
+app.use(async (req, res, next) => {
+    const sessionId = req.headers['session-id']
+
+    // if sessionId is missing
+    if(!sessionId) {
+        return next()
+    }
+
+    // if sessionId is present
+    const [data] = await db.select({
+        sessionId: userSessions.id,
+        id: usersTable.id,
+        userId: userSessions.userId,
+        name: usersTable.name,
+        email: usersTable.email
+    })
+    .from(userSessions)
+    .rightJoin(usersTable, eq(userSessions.userId, usersTable.id))
+    .where(eq(userSessions.id, sessionId))
+
+    // if there is no data or the sessionId was wrong so it couldnt get the data from the database
+    if(!data) {
+        return next()
+    }
+
+    req.user = data
+    next()
+})
 
 app.get('/', (req, res) => {
     return res.json({status: 'Server is up and running'})
--- session-based-authentication-with-drizzle/routes/users.routes.js
+++ session-based-authentication-with-drizzle/routes/users.routes.js
@@ -2,12 +2,37 @@ import express from "express"
 import signup from "../controllers/signup.controller.js"
 import login from "../controllers/login.controller.js"
 
+import { eq } from 'drizzle-orm'
+import db from '../db/index.js'
+import {usersTable, userSessions} from '../db/schema.js'
+
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    // TODO: Implement get current logged in user
-    res.status(501).json({ message: 'Not implemented' })
-}) // returns current loggedin user
+router.patch('/', async(req, res) => {
+    const user = req.user
+    if(!user) {
+        return res.statusCode(401).json({'error' : 'You are not logged in'})
+    }
+
+    const {name} = req.body
+
+    await db.update(usersTable).set({ name }).where(eq(usersTable.id, user.id))
+
+    return res.json({'status' : 'success'})
+})
+
+router.get('/', async (req, res) => {
+
+    const user = req.user
+    if(!user) {
+        return res.statusCode(401).json({'error' : 'You are not logged in'})
+    }
+
+    //if we get the data
+    return res.json({user})
+    
+})
+
 router.post('/signup', signup) // 
 router.post('/login', login)
 
