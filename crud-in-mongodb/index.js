cf6bc117ce320df8a0c8e17f10846f2f9cbd5e20
Ayush Patil <94798136+ayushpatil0810@users.noreply.github.com>
2026-01-18 23:02:04 +0530

    feat: add user authentication routes with signup and login functionality
    - Created a new package.json for the project with necessary dependencies.
    - Implemented user signup route to register new users with hashed passwords.
    - Implemented user login route to authenticate users and generate JWT tokens.

---
+++ crud-in-mongodb/index.js
@@ -0,0 +1,16 @@
+import 'dotenv/config'
+import express from "express"
+import { connectMongoDB } from "./connection.js"
+import userRouter from './routes/user.routes.js'
+
+const app = express()
+const PORT = process.env.PORT ?? 8000
+
+connectMongoDB(process.env.MONGODB_URL)
+    .then(() => console.log(`MongoDB Connected`))
+
+app.use(express.json())
+
+app.use('/user', userRouter)
+
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`))
